906d5fa77df3b0ba278ef16f432a2dca52d68600
MatadorProBr <71022656+MatadorProBr@users.noreply.github.com>
2021-04-08 16:03:37 -0300

    More organization in list of translations
    Thanks for XRevan86#0318 making this

--- src/renderer/language/languages.ts
+++ src/renderer/language/languages.ts
@@ -1,78 +1,30 @@
 import translationEN from '../../../static/locales/en/translation.json'
-import translationFR from '../../../static/locales/fr/translation.json'
-import translationES from '../../../static/locales/es/translation.json'
-import translationPTBR from '../../../static/locales/pt-BR/translation.json'
-import translationPTPT from '../../../static/locales/pt-PT/translation.json'
-import translationSK from '../../../static/locales/sk/translation.json'
-import translationSL from '../../../static/locales/sl/translation.json'
-import translationNL from '../../../static/locales/nl/translation.json'
-import translationDE from '../../../static/locales/de/translation.json'
-import translationID from '../../../static/locales/id/translation.json'
-import translationTR from '../../../static/locales/tr/translation.json'
-import translationRU from '../../../static/locales/ru/translation.json'
-import translationUK from '../../../static/locales/uk/translation.json'
 import translationAR from '../../../static/locales/ar/translation.json'
 import translationCA from '../../../static/locales/ca/translation.json'
 import translationCS from '../../../static/locales/cs/translation.json'
+import translationDE from '../../../static/locales/de/translation.json'
 import translationEO from '../../../static/locales/eo/translation.json'
+import translationES from '../../../static/locales/es/translation.json'
+import translationFR from '../../../static/locales/fr/translation.json'
+import translationID from '../../../static/locales/id/translation.json'
+import translationNL from '../../../static/locales/nl/translation.json'
 import translationPL from '../../../static/locales/pl/translation.json'
+import translationPTPT from '../../../static/locales/pt-PT/translation.json'
+import translationPTBR from '../../../static/locales/pt-BR/translation.json'
+import translationRU from '../../../static/locales/ru/translation.json'
+import translationSK from '../../../static/locales/sk/translation.json'
+import translationSL from '../../../static/locales/sl/translation.json'
+import translationTR from '../../../static/locales/tr/translation.json'
+import translationUK from '../../../static/locales/uk/translation.json'
 
 const languages = {
 	en: {
-	  translation: translationEN,
-	  name: 'English'
-	},
-	fr: {
-		translation : translationFR,
-		name: 'Français'
-	},
-	es: {
-		translation : translationES,
-		name: 'Español'
-	},
-	de: {
-		translation : translationDE,
-		name: 'Deutsch'
-	},
-	"pt-BR": {
-		translation : translationPTBR,
-		name: 'Português (BR)'
-	},
-	"pt-PT": {
-		translation : translationPTPT,
-		name: 'Português (PT)'
-	},
-	sk: {
-		translation : translationSK,
-		name: 'Slovenčina'
-	},
-	sl: {
-		translation : translationSL,
-		name: 'Slovenský'
-	},
-	nl: {
-		translation : translationNL,
-		name: 'Nederlands'
-	},
-	id: {
-		translation : translationID,
-		name: 'Indonesia'
-	},
-	tr: {
-		translation : translationTR,
-		name: 'Türkçe'
-	},
-	ru: {
-		translation : translationRU,
-		name: 'Русский'
-	},
-	uk: {
-		translation : translationUK,
-		name: 'Українська'
+		translation: translationEN,
+		name: 'English'
 	},
 	ar: {
 		translation : translationAR,
-		name: 'عربى'
+		name: 'العربية'
 	},
 	ca: {
 		translation : translationCA,
@@ -80,16 +32,64 @@ const languages = {
 	},
 	cs: {
 		translation : translationCS,
-		name: 'Txec'
+		name: 'Čeština'
+	},
+	de: {
+		translation : translationDE,
+		name: 'Deutsch'
 	},
 	eo: {
 		translation : translationEO,
 		name: 'Esperanto'
 	},
+	es: {
+		translation : translationES,
+		name: 'Español'
+	},
+	fr: {
+		translation : translationFR,
+		name: 'Français'
+	},
+	id: {
+		translation : translationID,
+		name: 'Indonesia'
+	},
 	pl: {
 		translation: translationPL,
-		name: 'Polskie'
+		name: 'Polski'
+	},
+	nl: {
+		translation : translationNL,
+		name: 'Nederlands'
+	},
+	"pt-PT": {
+		translation : translationPTPT,
+		name: 'Português (Portugal)'
+	},
+	"pt-BR": {
+		translation : translationPTBR,
+		name: 'Português (Brasil)'
+	},
+	ru: {
+		translation : translationRU,
+		name: 'Русский'
+	},
+	sk: {
+		translation : translationSK,
+		name: 'Slovenčina'
+	},
+	sl: {
+		translation : translationSL,
+		name: 'Slovenščina'
+	},
+	tr: {
+		translation : translationTR,
+		name: 'Türkçe'
+	},
+	uk: {
+		translation : translationUK,
+		name: 'Українська'
 	}
-  };
+};
 
 export default languages
