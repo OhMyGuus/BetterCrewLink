b5b2c2d1cd69a09668c873c690707d698fde49c4
OhMyGuus <info@guus.ninja>
2021-04-13 18:05:25 +0200

    ffs

--- src/common/Errors.ts
+++ src/common/Errors.ts
@@ -1,9 +1,4 @@
 export default {
-<<<<<<< HEAD
 	UNSUPPORTED_VERSION: 'Your version of Among Us is unsupported by BetterCrewLink.\n',
 	OPEN_AS_ADMINISTRATOR: "Couldn't connect to Among Us.\n" + 'Please re-open BetterCrewLink as Administrator.',
-=======
-	UNSUPPORTED_VERSION: 'Your version of Among Us is unsupported by CrewLink.\n',
-	OPEN_AS_ADMINISTRATOR: "Couldn't connect to Among Us.\n" + 'Please re-open CrewLink as Administrator.',
->>>>>>> origin/nightly
 };
