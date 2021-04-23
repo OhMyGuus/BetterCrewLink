b0c1339d37fac03fa41ff60ff31405e49e3c7251
MatadorProBr <71022656+MatadorProBr@users.noreply.github.com>
2021-04-23 09:28:54 -0300

    Organize the list of languages

--- src/renderer/language/languages.ts
+++ src/renderer/language/languages.ts
@@ -2,24 +2,24 @@ import translationEN from '../../../static/locales/en/translation.json';
 import translationAR from '../../../static/locales/ar/translation.json';
 import translationAREG from '../../../static/locales/ar-EG/translation.json';
 import translationCA from '../../../static/locales/ca/translation.json';
+import translationZHCN from '../../../static/locales/zh-CN/translation.json';
 import translationCS from '../../../static/locales/cs/translation.json';
-import translationDE from '../../../static/locales/de/translation.json';
-import translationEO from '../../../static/locales/eo/translation.json';
-import translationES from '../../../static/locales/es/translation.json';
-import translationFR from '../../../static/locales/fr/translation.json';
-import translationID from '../../../static/locales/id/translation.json';
 import translationNL from '../../../static/locales/nl/translation.json';
+import translationEO from '../../../static/locales/eo/translation.json';
+import translationFR from '../../../static/locales/fr/translation.json';
+import translationDE from '../../../static/locales/de/translation.json';
+import translationID from '../../../static/locales/id/translation.json';
+import translationJA from '../../../static/locales/ja/translation.json';
 import translationPL from '../../../static/locales/pl/translation.json';
 import translationPTPT from '../../../static/locales/pt-PT/translation.json';
 import translationPTBR from '../../../static/locales/pt-BR/translation.json';
 import translationRU from '../../../static/locales/ru/translation.json';
 import translationSK from '../../../static/locales/sk/translation.json';
 import translationSL from '../../../static/locales/sl/translation.json';
+import translationESES from '../../../static/locales/es-ES/translation.json';
+import translationSVSE from '../../../static/locales/sv-SE/translation.json';
 import translationTR from '../../../static/locales/tr/translation.json';
 import translationUK from '../../../static/locales/uk/translation.json';
-import translationSV from '../../../static/locales/sv/translation.json';
-import translationJA from '../../../static/locales/ja/translation.json';
-import translationZH from '../../../static/locales/zh-CN/translation.json';
 
 const languages = {
 	en: {
@@ -38,37 +38,41 @@ const languages = {
 		translation: translationCA,
 		name: 'Català',
 	},
+	'zh-CN': {
+		translation: translationZHCN, 
+		name: '汉语'
+	},
 	cs: {
 		translation: translationCS,
 		name: 'Čeština',
 	},
-	de: {
-		translation: translationDE,
-		name: 'Deutsch',
+	nl: {
+		translation: translationNL,
+		name: 'Nederlands',
 	},
 	eo: {
 		translation: translationEO,
 		name: 'Esperanto',
 	},
-	es: {
-		translation: translationES,
-		name: 'Español',
-	},
 	fr: {
 		translation: translationFR,
 		name: 'Français',
+	},
+	de: {
+		translation: translationDE,
+		name: 'Deutsch',
 	},
 	id: {
 		translation: translationID,
 		name: 'Bahasa Indonesia',
 	},
+	ja: {
+		translation: translationJA, 
+		name: '日本語'
+	},
 	pl: {
 		translation: translationPL,
 		name: 'Polski',
-	},
-	nl: {
-		translation: translationNL,
-		name: 'Nederlands',
 	},
 	'pt-PT': {
 		translation: translationPTPT,
@@ -90,6 +94,14 @@ const languages = {
 		translation: translationSL,
 		name: 'Slovenščina',
 	},
+	'es-ES': {
+		translation: translationES,
+		name: 'Español',
+	},
+	'sv-SE': {
+		translation: translationSV,
+		name: 'Svenska',
+	},
 	tr: {
 		translation: translationTR,
 		name: 'Türkçe',
@@ -97,18 +109,6 @@ const languages = {
 	uk: {
 		translation: translationUK,
 		name: 'Українська',
-	},
-	sv: {
-		translation: translationSV,
-		name: 'Svenska',
-	},
-	jp: {
-		translation: translationJA, 
-		name: '日本語'
-	},
-	zh: {
-		translation: translationZH, 
-		name: '汉语'
 	}
 };
 
