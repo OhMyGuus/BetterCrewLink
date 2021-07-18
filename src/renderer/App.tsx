469c70be7c5542f491c4221fedab54d13996eb69
Jayden Kohlman <2236351+JKohlman@users.noreply.github.com>
2021-07-18 15:19:42 -0600

    Fixed missing setting

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -175,6 +175,7 @@ export default function App({ t }): JSX.Element {
 			publicLobby_mods: 'NONE',
 		},
 		launchPlatform: GamePlatform.STEAM,
+        customPlatforms: {},
 	});
 	const lobbySettings = useReducer(lobbySettingsReducer, settings[0].localLobbySettings);
 
