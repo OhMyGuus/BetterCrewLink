e1e3f4ec7eed3a9f22d8ec9767f1c8c71a8f49d8
MatadorProBr <71022656+MatadorProBr@users.noreply.github.com>
2022-07-02 13:16:39 -1100

    Minor changes to the code (#321)

--- src/common/Mods.ts
+++ src/common/Mods.ts
@@ -2,8 +2,6 @@ export type ModsType =
 	| 'NONE'
 	| 'TOWN_OF_US'
 	| 'THE_OTHER_ROLES'
-	| 'THE_OTHER_ROLES_GM'
-	| 'TOWN_OF_IMPOSTORS'
 	| 'OTHER';
 
 export interface AmongusMod {
@@ -27,16 +25,6 @@ export const modList: AmongusMod[] = [
 		id: 'THE_OTHER_ROLES',
 		label: 'The Other Roles',
 		dllStartsWith: 'TheOtherRoles',
-	},
-	{
-		id: 'THE_OTHER_ROLES_GM',
-		label: 'The Other Roles: GM Edition',
-		dllStartsWith: 'TheOtherRolesGM',
-	},
-	{
-		id: 'TOWN_OF_IMPOSTORS',
-		label: 'Town of Impostors',
-		dllStartsWith: 'TownOfImpostors',
 	},
 	{
 		id: 'OTHER',
