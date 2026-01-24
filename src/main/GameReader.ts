53ebe0c13f3963e169f3946426ea9e345acab17e
August <coop@coopeeo.xyz>
2026-01-24 10:45:01 -0600

    Update GameReader.ts

--- src/main/GameReader.ts
+++ src/main/GameReader.ts
@@ -129,7 +129,7 @@ export default class GameReader {
 
 	getInstalledMods(filePath: string): AmongusMod {
 		const pathLower = filePath.toLowerCase();
-		alert(filepath);
+		alert(filePath);
 		if (pathLower.includes('?\\volume')) {
 			return modList[0];
 		} else {
